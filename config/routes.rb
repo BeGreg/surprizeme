62d8f3392bfc205cb7cee2e1a052c167ddd3d874
Gregory Blain <gregory.blain@gmail.com>
2017-12-05 13:59:57 +0100

    ajout device avant generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,13 @@
 Rails.application.routes.draw do
-  get 'surprises/index'
 
-  get 'surprises/new'
+  resources :users do
+    resources :ratings
+  end
 
-  get 'surprises/show'
-
-  get 'surprises/edit'
-
-  get 'ratings/new'
-
-  get 'ratings/edit'
-
-  get 'locations/index'
-
-  get 'locations/edit'
-
-  get 'locations/new'
-
-  get 'representations/new'
-
-  get 'moments/new'
-
-  get 'moments/show'
-
-  get 'moments/edit'
-
-  get 'moments/index'
-
-  get 'suppliers/index'
-
-  get 'suppliers/edit'
-
-  get 'products/new'
-
-  get 'products/show'
-
-  get 'products/edit'
-
-  get 'products/index'
-
-  get 'users/new'
-
-  get 'users/show'
-
-  get 'users/edit'
-
-  get 'users/index'
+  resources :products
+  resources :moments
+  resources :surprises
+  resources :location
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "starting Amazon"
+
+
+puts 'starting Nokogiri'
+
+selection = []
+url = "https://www.leroymerlin.fr/v3/search/search.do?resultOffset=0&resultLimit=50&resultListShape=PLAIN&keyword=bois"
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+html_doc.search('.prd-infos').search('h3').each do |element|
+  title = element.text.strip
+  print title
+  url = element.search('a').first.attr('href')
+  selection << { name: title, url: url }
+end
+
+# url = "https://www.amazon.fr/s/ref=sr_st_review-rank?keywords=gadget+insolite"
+# html_file = open(url).read
+# html_doc = Nokogiri::HTML(html_file)
+# html_doc.search('.s-item-container').each do |element|
+#   unless element.search('.a-icon-alt').nil?
+#     rating = "#{element.search('.a-icon-alt').text.strip.first}.#{element.search('.a-icon-alt').text.strip[2]}"
+#     if rating.to_i >= 4
+#       title = element.search('.s-access-title').text.strip
+#       puts title
+#       puts rating.to_is
+#     else
+#     end
+#   end
+# end
+#     print title
+#   url = element.search('a').first.attr('href')
+#   selection << { name: title, url: url }
+# end
