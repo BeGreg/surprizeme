e928cc6080acda6f27aafcf626a4e4aa3af0729f
Gregory Blain <gregory.blain@gmail.com>
2017-12-05 18:19:45 +0100

    intermediaire

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,37 @@
 
 puts "starting Amazon"
 
+require 'capybara/poltergeist'
+
+# Configure Poltergeist to not blow up on websites with js errors aka every website with js
+# See more options at https://github.com/teampoltergeist/poltergeist#customization
+Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, js_errors: false, :phantomjs => Phantomjs.path)
+  end
+
+  # Configure Capybara to use Poltergeist as the driver
+  Capybara.default_driver = :poltergeist
+
 selection = []
 
 browser = Capybara.current_session
-url = "https://www.amazon.fr/s/ref=sr_st_review-rank?keywords=gadget+insolite&rh=n%3A13921051%2Ck%3Agadget+insolite&qid=1512384836&__mk_fr_FR=%C3%85M%C3%85Z%C3%95%C3%91&sort=review-rank"
+url = "https://www.leroymerlin.fr/v3/search/search.do?resultOffset=0&resultLimit=50&resultListShape=PLAIN&keyword=bois"
 browser.visit url
-
-products = browser.all 's-item-container'
+products = browser.all '.prd-infos h3'
 
 products.each do |article|
-    puts article.all 'a-icon-alt'
+  title_element = article.all 'a'
+  puts title_element.html
+end
+
+# browser = Capybara.current_session
+# url = "https://www.amazon.fr/gadget-high-tech-High-Tech/s?ie=UTF8&page=1&rh=n%3A13921051%2Ck%3Agadget%20high%20tech"
+# browser.visit url
+# products = browser.all '.s-item-container'
+
+# products.each do |article|
+#     title_element = article.all '.a-icon-alt'
+#     puts title_element.html
 
   # has_css?
   # unless article.all 'a-icon-alt'.nil?
@@ -28,8 +49,20 @@ products.each do |article|
   #     puts rating.to_is
   #   end
   # end
-end
 #     print title
 #   url = element.search('a').first.attr('href')
 #   selection << { name: title, url: url }
 # end
+
+puts 'starting Nokogiri'
+
+selection = []
+url = "https://www.leroymerlin.fr/v3/search/search.do?resultOffset=0&resultLimit=50&resultListShape=PLAIN&keyword=bois"
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+html_doc.search('.prd-infos').search('h3').each do |element|
+  title = element.text.strip
+  print title
+  url = element.search('a').first.attr('href')
+  selection << { name: title, url: url }
+end
