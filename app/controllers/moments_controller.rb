e6a60639c704926422e7c09d6761111c4107a70e
Gregory Blain <gregory.blain@gmail.com>
2017-12-05 12:13:29 +0100

    ajout des controllers

---
+++ app/controllers/moments_controller.rb
@@ -0,0 +1,13 @@
+class MomentsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def index
+  end
+end
---
+++ app/controllers/surprises_controller.rb
@@ -0,0 +1,13 @@
+class SurprisesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
