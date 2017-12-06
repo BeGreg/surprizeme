19333d638557cfb570d73e9a1d6ca7de60201695
Marc Thiry <marc.thiry@edhec.com>
2017-12-06 18:49:00 +0100

    homepage & javafun

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :search
   def new
   end
 
@@ -10,4 +11,24 @@ class ProductsController < ApplicationController
 
   def index
   end
+
+
+  def search
+    @budget = params[:budget]
+    @gender = params[:gender]
+    @surprise_category = params[:surprise_category]
+    @productList = []
+    Product.all.each do |product|
+      if ((product.price + product.delivery_price) < @budget)  && product.gender == @gender && product.surprise_category ==  @surprise_category
+        @productList << product
+      end
+    p @productList
+    end
+  end
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :location
 
   get '/styleguide', to: 'pages#styleguide'
+  get 'search', to: 'products#search'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
