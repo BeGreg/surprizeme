65e9268e5e998cde97e2177da1d02736ef17a276
Gregory Blain <gregory.blain@gmail.com>
2017-12-06 18:06:52 +0100

    Ajout de la page hot-or-not

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: :hot_or_not
   def new
   end
 
@@ -14,7 +15,16 @@ class ProductsController < ApplicationController
 
   def hot_or_not
     @user = current_user
-    unrated_products = Rating.where.not(user_id: @user.id).products
-    @product = unrated_products.sample
+    # binding.pry
+    all_products = Product.all
+    all_products.to_a.delete_if { |product| Rating.where(product_id: product.id).size == 0}
+
+    # each do |product|
+    #   unless Rating.where(user_id: @user.id, product_id: product.id).size == 0
+    #     all_products.delete(product)
+    #   end
+    # end
+    @product = all_products.sample
+    authorize @product
   end
 end
