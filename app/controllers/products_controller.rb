43d1ce0a2286e05749b97b95d91eaa5cb87f67d0
Gregory Blain <gregory.blain@gmail.com>
2017-12-06 18:10:55 +0100

    fixing merge

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,19 +12,4 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
-
-  def hot_or_not
-    @user = current_user
-    # binding.pry
-    all_products = Product.all
-    all_products.to_a.delete_if { |product| Rating.where(product_id: product.id).size == 0}
-
-    # each do |product|
-    #   unless Rating.where(user_id: @user.id, product_id: product.id).size == 0
-    #     all_products.delete(product)
-    #   end
-    # end
-    @product = all_products.sample
-    authorize @product
-  end
 end
