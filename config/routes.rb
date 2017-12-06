93f1cc269301d34909a491a71a8de3fc6650801c
Gregory Blain <gregory.blain@gmail.com>
2017-12-06 15:39:24 +0100

    V1 du scrap Amazon

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   resources :surprises
   resources :location
 
+  post "hot_or_not/", to: "products#hot_or_not"
+
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
