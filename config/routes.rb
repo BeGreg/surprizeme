ad521c4f23a0f7761d55440d98cf68a8942bedd6
Laetitia Ahamada <alaetitia@live.fr>
2017-12-06 18:48:39 +0100

    template formulaire

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   resources :location
 
   get '/styleguide', to: 'pages#styleguide'
-
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
