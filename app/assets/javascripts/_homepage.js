7c1a6f914c3ae5bcdd85ee255a4c30ecb6fca5f3
Gregory Blain <gregory.blain@gmail.com>
2017-12-08 15:17:08 +0100

    var au lieu de const dans le js

--- app/assets/javascripts/_homepage.js
+++ app/assets/javascripts/_homepage.js
@@ -1,14 +1,14 @@
 function homepageAnimation() {
-  const product = document.querySelector(".product-title");
-  const product_form = document.querySelector(".product-form");
-  const moment = document.querySelector(".moment-title");
-  const moment_form = document.querySelector(".moment-form");
+  var product = document.querySelector(".product-title");
+  var product_form = document.querySelector(".product-form");
+  var moment = document.querySelector(".moment-title");
+  var moment_form = document.querySelector(".moment-form");
 
-  const product_title = document.querySelector(".product-title");
-  const moment_title = document.querySelector(".moment-title");
+  var product_title = document.querySelector(".product-title");
+  var moment_title = document.querySelector(".moment-title");
 
-  const headline = document.querySelector(".headline");
-  const headline_text = document.querySelector(".headline-text");
+  var headline = document.querySelector(".headline");
+  var headline_text = document.querySelector(".headline-text");
 
 
   // What happens when you click on "Product"
@@ -70,9 +70,9 @@ function homepageAnimation() {
 
 
   // what happens when you click on "Comment Ca marche ? "
-  const category_select = document.querySelectorAll(".category-select");
-  const howLink = document.getElementById("how-link");
-  const howItWorks = document.getElementById("how-it-works");
+  var category_select = document.querySelectorAll(".category-select");
+  var howLink = document.getElementById("how-link");
+  var howItWorks = document.getElementById("how-it-works");
   howLink.addEventListener("click", function() {
     howItWorks.classList.toggle("hide-how");
     howItWorks.classList.toggle("show-how");
