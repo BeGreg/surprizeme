1abac70563d34b42c2e83743e7acf87860643847
Marc Thiry <marc.thiry@edhec.com>
2017-12-08 13:58:20 +0100

    fix pour login & sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   before_action :authenticate_user!
   include Pundit
 
@@ -14,6 +16,14 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
   private
 
   def skip_pundit?
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20171208104842) do
-=======
-ActiveRecord::Schema.define(version: 20171207165627) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
