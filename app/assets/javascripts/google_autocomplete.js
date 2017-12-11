2a8d1c783cfa6d939b36df8019b34bde40423a2f
Gregory Blain <gregory.blain@gmail.com>
2017-12-11 17:04:18 +0100

    Autocomplétion pour bill_address et meilleur intrégation du script

--- app/assets/javascripts/google_autocomplete.js
+++ app/assets/javascripts/google_autocomplete.js
@@ -1,22 +1,27 @@
-function onPlaceChanged() {
+function onPlaceChanged(address_field) {
   var place = this.getPlace();
-  var delAddress = document.getElementById('surprise_del_address');
-  delAddress.blur();
-  delAddress.value = place.formatted_address;
+  var adress = document.getElementById(address_field);
+  address.blur();
+  address.value = place.formatted_address;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+  var billAddress = document.getElementById('surprise_bill_address');
   var delAddress = document.getElementById('surprise_del_address');
 
   if (delAddress) {
     var autocomplete = new google.maps.places.Autocomplete(delAddress, { types: ['geocode'], componentRestrictions: {country: 'fr'} });
-    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      onPlaceChanged('surprise_del_address')
+    });
   }
+
+  if (billAddress) {
+    var autocomplete = new google.maps.places.Autocomplete(billAddress, { types: ['geocode'], componentRestrictions: {country: 'fr'} });
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      onPlaceChanged('surprise_bill_address')
+    });
+  }
+
 });
 
-// var input = document.getElementById('searchTextField');
-// var options = {
-// restriction totale à la France
-//   types: ['(cities)'],
-//   componentRestrictions: {country: 'fr'}
-// };
