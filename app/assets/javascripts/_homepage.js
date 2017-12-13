726590af92f4227c9125426a288472060bbc8583
Marc Thiry <marc.thiry@edhec.com>
2017-12-13 18:19:17 +0100

    Homepage modification

--- app/assets/javascripts/_homepage.js
+++ app/assets/javascripts/_homepage.js
@@ -61,25 +61,20 @@ function homepageAnimation() {
   // what happense to Headline when interacting with page
   function HeadlineMove() {
     if (product_form.classList.contains("cat-show") || moment_form.classList.contains("cat-show")) {
-      headline.classList.add("move-up");
-      headline.classList.remove("move-down");
-      headline_text.classList.add("move-up");
-      headline_text.classList.remove("move-down");
+      headline.style.visibility = "hidden"
+      headline_text.style.visibility = "hidden"
+
 
     }else{
-      headline.classList.add("move-down");
-      headline.classList.remove("move-up");
-      headline_text.classList.add("move-down");
-      headline_text.classList.remove("move-up");
-      product_title.style.top ="25vh"
-      product_title.style.transition = "all 200ms"
+      headline.style.visibility = "visible"
+      headline_text.style.visibility = "visible"
     }
   };
 
   // what happense to Title when interacting with page
   function TitleMove(x, y, a, b) {
     if (x.classList.contains("cat-show")) {
-      a.style.top ="15vh"
+      a.style.top ="5vh"
       a.style.transition = "all 500ms"
       b.style.top ="25vh"
       b.style.transition = "all 500ms"
